a93a81251d792e65981ed166a0602347641c4f12
Arun Yadav <yarun4622@gmail.com>
2024-11-30 18:21:40 +0530

    change

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from("#content h1",{
+    y: 100,
+    duration: 1,
+    delay:0.2,
+    opacity:0
+})
+gsap.from("#page1 #img-div img ",{
+    y: 100,
+    duration: 1,
+    delay:0.2,
+    opacity:0
+})
+
